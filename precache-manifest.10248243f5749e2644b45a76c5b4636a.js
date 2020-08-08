d038d08866aa8dfdc19b1d65bcb51d01c71450a6
eduardo239 <eucrieiumaconta@gmail.com>
2020-08-08 15:33:00 -0300

    Updates

---
+++ precache-manifest.10248243f5749e2644b45a76c5b4636a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b50008040c645b97d45865a748b3c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "321c1bbeb05e7cabc5d7",
+    "url": "/static/css/main.ccf8376c.chunk.css"
+  },
+  {
+    "revision": "ad8df03cf0dfad92f648",
+    "url": "/static/js/2.72a38899.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72a38899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "321c1bbeb05e7cabc5d7",
+    "url": "/static/js/main.ddd7a0f1.chunk.js"
+  },
+  {
+    "revision": "d020ffac63d9c5ab2c94",
+    "url": "/static/js/runtime-main.ce36c70a.js"
+  },
+  {
+    "revision": "878fcdb101626c87b9865c448494526a",
+    "url": "/static/media/bg4.878fcdb1.svg"
+  },
+  {
+    "revision": "4fe0a017d5ca799788550edf60d49fc8",
+    "url": "/static/media/mcgill-library-V1IjTXINee0-unsplash.4fe0a017.jpg"
+  }
+]);
